88dbb6072890b71d16d5d45be57b7e6ae9b529aa
xQuantComputer <511574545@qq.com>
2021-02-19 16:55:57 +0800

    添加源码

---
+++ src/test/Test.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author saijun.yang
+ * @date 2021/2/19 16:06
+ * @description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+        Map map = new HashMap<>();
+        map.put("null","null");
+    }
+}
